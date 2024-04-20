bef8503f077f3da683a5e98d6c4b633e9dd063d6
BRUNA AMARO <brunaamarosilva2015@gmail.com>
2024-04-19 21:47:25 -0300

    continuação prog web 21:47

--- src/pages/CadastroUsuario/CadastroUsuario.jsx
+++ src/pages/CadastroUsuario/CadastroUsuario.jsx
@@ -10,27 +10,35 @@ const CadastroUsuario = () => {
       confSenha: ''
     }//OBJETO
   )
+  const alteraFormulario = (e) => {
+    const { name, value } = e.target;
+    setFormulario({ ...formulario, [name]: value })
+  }
+
 
   return (
     <div>
       <h2>Cadastro de Usuário</h2>
       <form>
         <label htmlFor='nome' >Nome</label>
-        <input type='text' id="nome"
+        <input type='text' id="nome" name='nome'
           value={formulario.nome}
-          onChange={ } />
+          onChange={alteraFormulario} />
 
         <label htmlFor='email' >Email</label>
-        <input type='text' id="email"
-          value={formulario.emai} />
+        <input type='text' id="email" name='email'
+          value={formulario.emai} 
+          onChange={alteraFormulario} />
 
         <label htmlFor='senha' >Senha</label>
-        <input type='text' id="senha"
-          value={formulario.senha} />
+        <input type='text' id="senha" name='senha'
+          value={formulario.senha} 
+          onChange={alteraFormulario} />
 
         <label htmlFor='confSenha' >Confirmar Senha</label>
-        <input type='text' id="confSenha"
-          value={formulario.confSenha} />
+        <input type='text' id="confSenha" name='confSenha'
+          value={formulario.confSenha}
+          onChange={alteraFormulario} />
 
         <button type='submit'>Salvar</button>
       </form>
